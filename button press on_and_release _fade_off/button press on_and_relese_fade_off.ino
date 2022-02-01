12f2c8ec4014d5e471c03a93ab820dc7b9c4787e
§T4Rk~9000 <stark9000@gmail.com>
2022-02-01 18:22:50 +0530

    Add files via upload

---
+++ button press on_and_release _fade_off/button press on_and_relese_fade_off.ino
@@ -0,0 +1,41 @@
+const int buttonPin = 8;
+const int led = 11;
+int brightness = 255 ;
+boolean pressed = false;
+
+
+void setup () {
+  pinMode (led, OUTPUT );
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+void loop() {
+  if (digitalRead (buttonPin) == LOW) {
+    digitalWrite(led, HIGH);
+    brightness = 255;
+    pressed = true;
+  } else {
+    if (pressed == true) {
+      brightness = brightness - 5;
+      delay(50);
+      analogWrite(led, brightness );
+      if (brightness < 0) {
+        brightness = 0;
+        digitalWrite(led, LOW);
+        pressed = false;
+      }
+    }
+  }
+}
+
+
+
+void test() {
+  if (digitalRead(buttonPin) == HIGH)
+    analogWrite(led, brightness );
+  brightness = brightness - 5;
+  if (brightness <= 255) {
+    brightness = 0;
+  }
+  delay(10);
+
+}
